83cd9a9b4a884cb6e42061f13703813c49506c5e
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-10-11 20:08:39 -0700

    Time: 72 ms (31.40%), Space: 16.4 MB (79.05%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hash={}
+        for i in range(97,123):
+            hash[i]=0
+        for x in magazine:
+            hash[ord(x)]+=1
+        for y in ransomNote:
+            hash[ord(y)]-=1
+        for i in range(97,123):
+            if hash[i]<0:
+                return False
+        else:
+            return True
