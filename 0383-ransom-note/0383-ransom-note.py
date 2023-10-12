bf50805d0d530e5e4e1c1e84f069bde1e318275a
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-10-11 20:10:19 -0700

    Time: 58 ms (66.60%), Space: 16.4 MB (79.05%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,14 +1,19 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        hash={}
-        for i in range(97,123):
-            hash[i]=0
-        for x in magazine:
-            hash[ord(x)]+=1
-        for y in ransomNote:
-            hash[ord(y)]-=1
-        for i in range(97,123):
-            if hash[i]<0:
-                return False
-        else:
+        # hash={}
+        # for i in range(97,123):
+        #     hash[i]=0
+        # for x in magazine:
+        #     hash[ord(x)]+=1
+        # for y in ransomNote:
+        #     hash[ord(y)]-=1
+        # for i in range(97,123):
+        #     if hash[i]<0:
+        #         return False
+        # else:
+        #     return True
+        rans, maga = Counter(ransomNote), Counter(magazine)
+        if rans & maga == rans:
             return True
+        else:
+            return False
