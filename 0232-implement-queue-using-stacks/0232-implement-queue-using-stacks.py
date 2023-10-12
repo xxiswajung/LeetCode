944d312669734bebb2830b966dbdf2a281cfc47a
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-10-11 17:14:39 -0700

    Time: 40 ms (43.55%), Space: 16.4 MB (56.92%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,33 +1,33 @@
 class MyQueue:
 
     def __init__(self):
-        self.push_stack = []
-        self.pop_stack = []
+        self.input = []
+        self.output = []
 
     def push(self, x: int) -> None:
-        self.push_stack.append(x)
+        self.input.append(x)
 
     def pop(self) -> int:
         if self.empty(): return #push stack & pop stack 둘다 비었으면 pop 중지
-        if len(self.pop_stack): #pop stack 에 원소가 있다면
-            return self.pop_stack.pop() #pop
+        if len(self.output): #pop stack 에 원소가 있다면
+            return self.output.pop() #pop
         else: # pop stack 에만 q 원소가 없다
-            while len(self.push_stack):
-                self.pop_stack.append(self.push_stack.pop())
-        return self.pop_stack.pop()
+            while len(self.input):
+                self.output.append(self.input.pop())
+        return self.output.pop()
 
 
     def peek(self) -> int:
         if self.empty(): return
-        if len(self.pop_stack):
-            return self.pop_stack[-1]
+        if len(self.output):
+            return self.output[-1]
         else:
-            while len(self.push_stack):
-                self.pop_stack.append(self.push_stack.pop())
-        return self.pop_stack[-1]
+            while len(self.input):
+                self.output.append(self.input.pop())
+        return self.output[-1]
 
     def empty(self) -> bool:
-        if self.push_stack or self.pop_stack:
+        if self.input or self.output:
             return False
         else:
             return True
