ae0213a4fa4c7b5025c8ff9d0cf921ba1ba70fb6
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-10-12 22:27:19 -0700

    Time: 43 ms (50.92%), Space: 18 MB (29.17%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,13 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        new_list = None
-        current = head
-
-        while current:
-            next_node = current.next
-            current.next = new_list
-            new_list = current
-            current = next_node
-
-        return new_list
+        new=None
+        cur=head
+        
+        while cur:
+            next_n=cur.next #cur의 다음 노드를 지시하는 next를 한 칸 전진
+            cur.next=new #reverse
+            new=cur #cur의 전 노드를 지시하는 new를 한 칸 전진, new: 2->1
+            cur=next_n #cur 한칸 전진
+        return new
