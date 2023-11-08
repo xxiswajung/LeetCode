7a4d35b32cd81bd5f63e2619f036a4554dfc7754
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-11-08 11:18:50 -0800

    Time: 41 ms (90.81%), Space: 16.6 MB (41.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -4,31 +4,25 @@ class Solution:
         if length<1:
             return -1
         
-        def BST(left, right, nums, target):
+        def BST(left,right,nums,target):
             while left<=right:
                 mid=(left+right)//2
                 if nums[mid]==target:
                     return mid
-                #mid를 기준으로 왼편이 정렬된 배열
-                if nums[left]<=nums[mid]:
-                    #target이 왼편에 있을 때
-                    if nums[left]<=target and target<nums[mid]:
-                        right=mid-1
-                    #target이 왼편에 없을 때
-                    else:
-                        left=mid+1
-                #mid를 기준으로 오른편이 정렬된 배열
                 else:
-                    #target이 오른편에 있을 때
-                    if nums[mid]<target and target<=nums[right]:
-                        left=mid+1
-                    #target이 왼편에 있을 때 
+                    if nums[left]<=nums[mid]:
+                        if nums[left]<=target and target<nums[mid]:
+                            right=mid-1
+                        else:
+                            left=mid+1
                     else:
-                        right=mid-1
+                        if nums[mid]<target and target<=nums[right]:
+                            left=mid+1
+                        else:
+                            right=mid-1
             return -1
         
-        return BST(0, length-1, nums, target)
-    
+        return BST(0,length-1,nums,target)
         
         
                     
