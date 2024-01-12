3ac868a7ca88c0019ca3656eee992ab12a7efe4d
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-01-11 16:36:48 -0800

    Time: 671 ms (86.39%), Space: 21.6 MB (16.60%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,24 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans=[]
+        ans = []   
         nums.sort()
-        
-        for i in range(len(nums)-2):
-            j,k=i+1,len(nums)-1
-            if i>0 and nums[i]==nums[i-1]:
+        #nums=[-4,-1,-1,0,1,2]
+        for i in range(len(nums)):
+            # 정렬을 했는데도 현재 값과 이전 값이 같으면 볼 필요 없음, 0이 안됨
+            if i!=0 and nums[i]==nums[i-1]:
                 continue
-            while j<k:
-                sum=nums[i]+nums[j]+nums[k]
-                if sum>0:
-                    k-=1
-                elif sum<0:
-                    j+=1
+            l=i+1
+            r=len(nums)-1
+            while l<r:
+                total=nums[i]+nums[l]+nums[r]
+                if total==0:
+                    ans.append([nums[i],nums[l],nums[r]])
+                    l+=1
+                    #중복 피하기 위해
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+                elif total<0:
+                    l+=1
                 else:
-                    ans.append([nums[i],nums[j],nums[k]])
-                    #중복제거, nums[i]와 nums[j]는 이미 정답에 넣었으니 같은 값들을 pass
-                    while j<k and nums[j]==nums[j+1]:
-                        j+=1
-                    while j<k and nums[k]==nums[k-1]:
-                        k-=1
-                    #중복제거 후 새로운 값 비교를 위한 값 갱신
-                    j+=1
-                    k-=1
+                    r-=1
         return ans
-                
