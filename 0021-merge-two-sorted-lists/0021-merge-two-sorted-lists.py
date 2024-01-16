72fcc62928a968a48f65b88f5a93a4d684bbfeaa
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-01-16 15:48:07 -0800

    Time: 38 ms (79.87%), Space: 17.4 MB (6.32%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,20 +5,19 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        node = ListNode() #헤더(맨 앞) 선언해서 연결리스트 생성
-        cur = node #얘로 연결리스트를 끝까지 만들예정
+        prehead = ListNode(-1)
+        prev = prehead
+        
         while list1 and list2:
-            if list1.val <= list2.val:
-                cur.next = list1
-                list1 = list1.next
+            if list1.val<=list2.val:
+                prev.next=list1
+                list1=list1.next
             else:
-                cur.next = list2
-                list2 = list2.next
-            cur = cur.next
-        cur.next = list1 or list2
-        #cur 의 역할 : node가 해야 할 일(=다른 노드 잇는 일)을 대신해주되, 맨 앞으로 못 가기 때문에 전체 리스트를 출력X
-        #node의 역할 : 따라서 맨 앞자리를 지키고 있는 node를 남겨두고, cur로 복사해야함
-        return node.next
-            
-
-                    
+                prev.next=list2
+                list2=list2.next
+            prev=prev.next
+        
+        prev.next=list1 or list2
+        return prehead.next
+        
+                
