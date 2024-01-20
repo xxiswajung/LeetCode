e6d1b692be3af4484344afb49a7830f49b0dd1c5
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-01-19 16:20:46 -0800

    Time: 53 ms (89.38%), Space: 20.3 MB (99.97%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,24 +7,11 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        stack = [root]
-        parent = {root: None} #make a dictionary to record the node's parent. The root node is parent itself.
-        
-        while stack:
-            node = stack.pop()
-            if node.left:
-                parent[node.left] = node
-                stack.append(node.left)
-            if node.right:
-                parent[node.right] = node
-                stack.append(node.right)
-        
-        ancestor = set()
-        while p :
-            ancestor.add(p)
-            p = parent[p] #find p's parent from dictionary
-        while q not in ancestor: #repeat it while q finds the ancestors from p's ancestors
-            q = parent[q]
-        
-        return q
-        
+        if p.val<root.val and q.val<root.val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        elif p.val>root.val and q.val>root.val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        else:
+            return root
+
+                    
