9d0abe3b8c6adb13080c0096498dcf6896bf5e46
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-01-25 12:03:59 -0800

    Time: 74 ms (44.75%), Space: 17.1 MB (54.67%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,17 +1,22 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        def DFS(x,y,aa):
+        visited=[[0]*len(image[0]) for _ in range(len(image))]
+        dx=[-1,0,1,0]
+        dy=[0,1,0,-1]
+
+        def DFS(x,y):
             for i in range(4):
                 xx=x+dx[i]
                 yy=y+dy[i]
-                if 0<=xx<len(image) and 0<=yy<len(image[0]) and image[xx][yy]==aa and visit[xx][yy]==0:
-                    image[xx][yy]=color
-                    visit[xx][yy]=1
-                    DFS(xx,yy,aa)
-        dx=[-1,0,1,0]
-        dy=[0,1,0,-1]            
-        visit=[[0]*(len(image[0])) for _ in range(len(image))] 
-        visit[sr][sc]=1
-        DFS(sr,sc,image[sr][sc])
-        image[sr][sc]=color
+                if 0<=xx<len(image) and 0<=yy<len(image[0]) and visited[xx][yy]==0 and image[xx][yy]==image[sr][sc]:
+                    visited[xx][yy]=1
+                    DFS(xx,yy)
+                    
+        visited[sr][sc]=1
+        DFS(sr,sc)
+        
+        for x in range(len(image)):
+            for y in range(len(image[0])):
+                if visited[x][y]==1:
+                    image[x][y]=color
         return image
