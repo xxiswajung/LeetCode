6db5dbd2294026e54559a4e9d404eb0e96f0d500
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-01-26 15:30:21 -0800

    Time: 29 ms (91.68%), Space: 16.5 MB (80.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,20 +1,11 @@
-class Solution(object):
-    def climbStairs(self, n):
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dy=[0]*(n+1)
+        dy[1]=1
         if n==1 or n==2:
             return n
-        dp = [-1]*(n+1)
-        dp[1],dp[2]=1,2
-        for i in range(3,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n]
-        
-# class Solution(object):
-#     def climbStairs(self,n):
-#         def climb(n):
-#             if n in memo:
-#                 return memo[n]
-#             else:
-#                 memo[n]=climb(n-1)+climb(n-2)
-#                 return memo[n]
-#         memo = {1:1, 2:2} #use dictionary
-#         return climb(n)
+        else:
+            dy[2]=2
+            for i in range(3,n+1):
+                dy[i]=dy[i-2]+dy[i-1]
+            return dy[n]
