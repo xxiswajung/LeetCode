33d41ec82708e0b73cb14b5211a2447543a98474
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-01-26 14:53:49 -0800

    Time: 28 ms (94.57%), Space: 16.5 MB (66.63%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,36 +1,24 @@
 class MyQueue:
 
     def __init__(self):
-        self.input = []
-        self.output = []
+        self.stack=[]
 
     def push(self, x: int) -> None:
-        self.input.append(x)
+        self.stack.append(x)
 
     def pop(self) -> int:
-        if self.empty(): return #push stack & pop stack 둘다 비었으면 pop 중지
-        if len(self.output): #pop stack 에 원소가 있다면
-            return self.output.pop() #pop
-        else: # pop stack 에만 q 원소가 없다
-            while len(self.input):
-                self.output.append(self.input.pop())
-        return self.output.pop()
-
+        item=self.stack[0]
+        self.stack=self.stack[1:]
+        return item
 
     def peek(self) -> int:
-        if self.empty(): return
-        if len(self.output):
-            return self.output[-1]
-        else:
-            while len(self.input):
-                self.output.append(self.input.pop())
-        return self.output[-1]
+        return self.stack[0]
 
     def empty(self) -> bool:
-        if self.input or self.output:
-            return False
-        else:
+        if len(self.stack)==0:
             return True
+        else:
+            return False
 
 
 # Your MyQueue object will be instantiated and called as such:
