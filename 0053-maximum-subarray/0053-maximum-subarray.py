343247640d452c4fa9f0d1db695bb7fb68acc1d6
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-07 23:14:43 -0800

    Time: 569 ms (38.95%), Space: 31 MB (65.13%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,20 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-#         ans = nums[0]
-#         summ = 0
-
-#         for num in nums:
-#             if summ<0:
-#                 summ=0
-#             summ+=num
-#             ans = max(ans, summ)
-
-#         return ans
-
-        ans = -100000
-        summ = 0
+        max_sum=0
+        result_sum=nums[0]
         
+        #idea
+        #1: 현재 변수가 현재누적합보다 크면, 더 큰 수인 현재 변수로 시작포인트를 갱신해야한다!!
+        #2: 음수는 누적합에 도움이 안되기 때문에, 합이 작아지면 갱신해야한다는 것이 포인트
+        #3: 현재누적합보다 최종누적합이 더 큰 경우는, 배열을 늘릴수록 maximum에 도움이 안된다는 뜻
         for num in nums:
-            summ = max(summ+num, num)
-            ans = max(ans,summ)
-        return ans
+            max_sum=max(max_sum+num,num) #어디서부터 시작할지 결정하는 변수
+            result_sum=max(max_sum, result_sum) #최종적으로 누적된 결과를 저장하는 변수
+        return result_sum
