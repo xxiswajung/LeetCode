3a184e35dd877aa92158d8d6995b1454494da542
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-14 12:00:27 -0800

    Time: 711 ms (57.04%), Space: 20.8 MB (58.67%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []   
+        answer=[]
         nums.sort()
-        #nums=[-4,-1,-1,0,1,2]
-        for i in range(len(nums)-2):
-            # 정렬을 했는데도 현재 값과 이전 값이 같으면 볼 필요 없음, 0이 안됨
+        
+        for i in range(len(nums)):
             if i!=0 and nums[i]==nums[i-1]:
                 continue
             l=i+1
             r=len(nums)-1
+            
             while l<r:
                 total=nums[i]+nums[l]+nums[r]
                 if total==0:
-                    ans.append([nums[i],nums[l],nums[r]])
-                    #중복 피하기 위해
-                    while l<r and nums[l]==nums[l+1]:
-                        l+=1
-                    while l<r and nums[r]==nums[r-1]:
-                        r-=1
+                    answer.append([nums[i],nums[l],nums[r]])
                     l+=1
-                    r-=1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
                 elif total<0:
                     l+=1
                 else:
                     r-=1
-        return ans
+        return answer
+        
+            
+            
