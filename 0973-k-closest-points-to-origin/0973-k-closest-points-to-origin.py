befa37a79647abd25b3cc4ba064ec69502c6b060
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-13 16:09:05 -0800

    Time: 594 ms (69.61%), Space: 23.2 MB (52.29%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -5,5 +5,6 @@ class Solution:
             if len(heap)<k:
                 heapq.heappush(heap,[-(x**2+y**2),[x,y]])
             else:
-                heapq.heappushpop(heap,[-(x**2+y**2),[x,y]])
-        return [pair for value, pair in heap]
+                heapq.heappushpop(heap,[-(x**2+y**2),[x,y]]) #가장 작은 것을 pop하고 item 을 push
+                #거리를 음수로 표현하면 가장 작은 수가 곧 가장 먼 거리이므로 가장 작은 수를 pop 하는게 맞음
+        return [coordinate for distance, coordinate in heap]
