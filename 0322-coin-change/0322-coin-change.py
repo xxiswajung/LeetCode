129dce50167672cedd2edc05cb7d53fd3c3f7da5
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-20 15:31:10 -0800

    Time: 711 ms (88.95%), Space: 16.9 MB (94.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,14 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dy=[999999]*(amount+1)
+        init=inf
+        dy=[init]*(amount+1)
         dy[0]=0
         if min(coins)>amount and amount!=0:
             return -1
         for x in coins:
-            for y in range(1,len(dy)):
-                if y>=x:
-                    dy[y]=min(dy[y],dy[y-x]+1)
-        if dy[amount]==999999:
+            for y in range(x,len(dy)):
+                dy[y]=min(dy[y],dy[y-x]+1)
+        if dy[amount]==inf:
             return -1
         else:
             return dy[amount]
