5dd37d9fb36c914f0a0052175450e5845cc62fca
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-20 16:11:56 -0800

    Time: 172 ms (41.37%), Space: 23.9 MB (73.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,15 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        answer=[1]*len(nums)
-        temp=1
+        answer=[1]*(len(nums))
+        tmp=1
         
-        #자신의 왼쪽에 있는 수 곱하기
         for i in range(len(nums)):
-            answer[i]*=temp
-            temp*=nums[i]
-        temp=1
-        #자신의 오른쪽에 있는 수 곱하기
+            answer[i]*=tmp
+            tmp*=nums[i] #본인의 왼쪽 수
+        tmp=1
         for i in range(len(nums)-1,-1,-1):
-            answer[i]*=temp
-            temp*=nums[i]
+            answer[i]*=tmp
+            tmp*=nums[i]
         return answer
