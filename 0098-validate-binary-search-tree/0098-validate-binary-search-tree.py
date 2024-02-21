ba6857c3889354bb976cf5440c758d108e1a6492
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-21 15:37:14 -0800

    Time: 39 ms (76.82%), Space: 18.3 MB (90.45%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,13 +6,10 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return self.Valid(root, float("-inf"), float("inf"))
+        return self.Valid(root, float('-inf'), float('inf'))
     def Valid(self, root, left, right) -> bool:
         if not root:
             return True
-        if not (root.val < right and root.val > left):
+        if not (root.val<right and root.val>left):
             return False
         return self.Valid(root.left, left, root.val) and self.Valid(root.right, root.val, right)
-        # left subtree : 부모트리보다 작으므로 오른쪽 boundary 갱신 -> right = root.val
-        # right subtree : 부모트리보다 크므로 왼쪽 boundary 갱신 -> left = root.val
-        
