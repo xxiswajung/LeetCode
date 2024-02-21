15c01c7926e8fe91005a6b47e80610fcfdd78fa7
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-21 15:51:12 -0800

    Time: 248 ms (58.67%), Space: 19.2 MB (69.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,30 +2,23 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         dx=[-1,0,1,0]
         dy=[0,1,0,-1]
-        m=len(grid)
-        n=len(grid[0])
-        visit=[['0']*n for _ in range(m)]
-
+        xlen=len(grid)
+        ylen=len(grid[0])
+        visited=[[0]*ylen for _ in range(xlen)]
+        
         def DFS(x,y):
-            global cnt
             for i in range(4):
                 xx=x+dx[i]
                 yy=y+dy[i]
-                if 0<=xx<m and 0<=yy<n and visit[xx][yy]=='0' and grid[xx][yy]=='1':
-                    visit[xx][yy]=cnt
-                    grid[xx][yy]='0'
+                if 0<=xx<xlen and 0<=yy<ylen and visited[xx][yy]==0 and grid[xx][yy]=="1":
+                    visited[xx][yy]=1
                     DFS(xx,yy)
-                    
-            
+                    grid[xx][yy]="0"
         
-        global cnt
-        cnt=1
-        for x in range(m):
-            for y in range(n):
-                if grid[x][y]=='1':
-                    visit[x][y]=cnt
-                    grid[x][y]='0'
+        cnt=0
+        for x in range(xlen):
+            for y in range(ylen):
+                if grid[x][y]=="1":
                     DFS(x,y)
                     cnt+=1
-        return cnt-1
-            
+        return cnt
