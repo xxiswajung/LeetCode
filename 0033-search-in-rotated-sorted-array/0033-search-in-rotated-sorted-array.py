3a6aede1a8ac503a54696fc6dee0886e302e6949
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-22 11:06:23 -0800

    Time: 53 ms (12.93%), Space: 17 MB (75.63%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,28 +1,3 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        length=len(nums)
-        if length<1:
-            return -1
-        
-        def BST(left,right,nums,target):
-            while left<=right:
-                mid=(left+right)//2
-                if nums[mid]==target:
-                    return mid
-                else:
-                    if nums[left]<=nums[mid]:
-                        if nums[left]<=target and target<nums[mid]:
-                            right=mid-1
-                        else:
-                            left=mid+1
-                    else:
-                        if nums[mid]<target and target<=nums[right]:
-                            left=mid+1
-                        else:
-                            right=mid-1
-            return -1
-        
-        return BST(0,length-1,nums,target)
-        
-        
-                    
+        return nums.index(target) if target in nums else -1
