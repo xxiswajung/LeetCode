225b489687903e660ea09c0a81088de1c7cab482
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-21 16:30:32 -0800

    Time: 45 ms (87.78%), Space: 16.6 MB (81.75%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,40 +1,32 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        global answer
-        answer = 0
-        m=len(grid)
-        n=len(grid[0])
         dx=[-1,0,1,0]
         dy=[0,1,0,-1]
-        minute = [[0]*n for _ in range(m)]
+        xlen=len(grid)
+        ylen=len(grid[0])
+        result=0
+        visited=[[0]*ylen for _ in range(xlen)]
         Q=deque()
-        
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]==2:
-                    Q.append((i,j))
+        children=[]
+        for x in range(xlen):
+            for y in range(ylen):
+                if grid[x][y]==2:
+                    Q.append((x,y,0))
         
         while Q:
-            x,y=Q.popleft()
+            x,y,minute=Q.popleft()
+            result=max(minute,result)
             for i in range(4):
                 xx=x+dx[i]
                 yy=y+dy[i]
-                if 0<=xx<m and 0<=yy<n and grid[xx][yy]==1:
+                if 0<=xx<xlen and 0<=yy<ylen and visited[xx][yy]==0 and grid[xx][yy]==1:
+                    visited[xx][yy]=visited[x][y]+1
                     grid[xx][yy]=2
-                    minute[xx][yy]=minute[x][y]+1
-                    Q.append((xx,yy))
-        
-        flag=1
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]==1:
-                    flag=0
-        
-        if flag==1:
-            for i in range(m):
-                for j in range(n):
-                    if minute[i][j]>answer:
-                        answer=minute[i][j]
-            return answer
+                    Q.append((xx,yy,minute+1))
+                    
+        for x in range(xlen):
+            for y in range(ylen):
+                if grid[x][y]==1:
+                    return -1
         else:
-            return -1
+            return result
