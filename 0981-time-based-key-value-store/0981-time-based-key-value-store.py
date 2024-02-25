decbdd9c1758e45f324b0daef5b734b40a108551
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-24 16:20:02 -0800

    Time: 511 ms (92.39%), Space: 74.4 MB (39.83%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -7,37 +7,11 @@ class TimeMap:
         self.tmap[key].append([value,timestamp])
             
     def get(self, key: str, timestamp: int) -> str:
-        start=0
-        end=len(self.tmap[key])-1
-        if self.tmap[key] == []:
-            return ''
-        if self.tmap[key][0][1]>timestamp:
-            return ''
-        else:
-            while start<=end:
-                mid=(start+end)//2
-
-                if self.tmap[key][mid][1]==timestamp:
-                    return self.tmap[key][mid][0]
-                elif self.tmap[key][mid][1]>timestamp:
-                    end=mid-1
-                else:
-                    start=mid+1
-            if self.tmap[key][mid][1]<=timestamp:
-                return self.tmap[key][mid][0]
-            else:
-                if self.tmap[key][mid-1][0] is None:
-                    return ''
-                else:
-                    return self.tmap[key][mid-1][0]
-        
-    
-        # for v, t in self.tmap[key]:
-        #     if t>timestamp:
-        #         break
-        #     else:
-        #         answer=v
-        # return answer
+        for i in range(len(self.tmap[key])-1,-1,-1):
+            v, t=self.tmap[key][i]
+            if t<=timestamp:
+                return v
+        return ''
 
 
 # Your TimeMap object will be instantiated and called as such:
