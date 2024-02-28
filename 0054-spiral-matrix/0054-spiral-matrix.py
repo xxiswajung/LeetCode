21b806db821785a7e24196c8f30a9897e6d8d25e
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-27 20:26:46 -0800

    Time: 27 ms (95.07%), Space: 16.6 MB (46.88%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,36 +1,45 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        # m=len(matrix[0])
-        # n=len(matrix)
-        # answer=[]
-        # i=0
-        # while len(answer)!=m*n:
-        #     for p in range(i,m-i-1):
-        #         answer.append(matrix[i][p])
-        #     for q in range(i,n-i-1):
-        #         answer.append(matrix[q][m-i-1])
-        #     for p in range(m-i-1,i,-1):
-        #         answer.append(matrix[n-i-1][p])
-        #     for q in range(n-i-1,i,-1):
-        #         answer.append(matrix[q][i])
-        #     i+=1
-        # return answer
+#         answer=[]
         
+#         while matrix:
+#             answer+=matrix.pop(0)
+            
+#             if matrix and matrix[0]:
+#                 for row in matrix:
+#                     answer.append(row.pop())
+            
+#             if matrix:
+#                 answer+=matrix.pop()[::-1]
+            
+#             if matrix and matrix[0]:
+#                 for row in matrix[::-1]:
+#                     answer.append(row.pop(0))
+        
+#         return answer
+        dd=[(-1,0),(0,1),(1,0),(0,-1)]
+        n,m=len(matrix),len(matrix[0])
+        visited=[[0]*m for _ in range(n)]
+        x,y,d=0,0,1
+        visited[0][0]=1
         answer=[]
+        answer.append(matrix[0][0])
         
-        while matrix:
-            answer+=matrix.pop(0)
+        while True:
+            if len(answer)==n*m:
+                break
+                
+            xx=x+dd[d][0]
+            yy=y+dd[d][1]
+
+            if xx<0 or xx>=n or yy>=m or yy<0 or visited[xx][yy]==1:
+                d=(d+1)%4
+                continue
             
-            if matrix and matrix[0]:
-                for row in matrix:
-                    answer.append(row.pop())
-            
-            if matrix:
-                answer+=matrix.pop()[::-1]
-            
-            if matrix and matrix[0]:
-                for row in matrix[::-1]:
-                    answer.append(row.pop(0))
+            answer.append(matrix[xx][yy])
+            visited[xx][yy]=1
+            x,y=xx,yy
         
         return answer
+                    
         
