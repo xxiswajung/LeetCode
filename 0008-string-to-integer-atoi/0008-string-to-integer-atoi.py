914633f738081249b294565a0afcfea54151d942
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-27 19:29:29 -0800

    Time: 43 ms (29.90%), Space: 16.7 MB (57.66%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,30 +1,33 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        ans=''
-        number=['0','1','2','3','4','5','6','7','8','9']
-        symbol=['++','--','+-','-+']
-        INT_MIN = -2**31
-        INT_MAX = 2**31 - 1
-        s=s.strip()
-        if not s:
-            return 0
-        
-        sign=1
-        if s[0] in ['+','-']:
-            if s[0]=='-':
-                sign=-1
-            s=s[1:]
-        
-        result=0
-        for char in s:
-            if not char.isdigit():
+        pos=1
+        sign=False
+        dig=False
+        answer=''
+        for x in s:
+            if (sign is True or dig is True) and x.isdigit() is False:
+                    break
+            if x=='+':
+                pos=1
+                sign=True
+            elif x=='-':
+                pos=-1
+                sign=True
+            elif x.isdigit():
+                answer+=x
+                dig=True
+            elif x==' ':
+                continue
+            else:
                 break
-            result=result*10+int(char)
-        result=sign*result
-        
-        if result > INT_MAX:
-            return INT_MAX
-        elif result < INT_MIN:
-            return INT_MIN
+        if answer=='':
+            return 0
         else:
-            return result
+            ans=pos*int(answer)
+            if ans<-math.pow(2,31):
+                return -int(math.pow(2,31))
+            elif ans>math.pow(2,31)-1:
+                return int(math.pow(2,31)-1)
+            else:
+                return ans
+            
