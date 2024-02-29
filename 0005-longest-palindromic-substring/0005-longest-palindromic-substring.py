63b37760ec8dc4e026106762f50e10da33e23672
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-02-28 17:13:49 -0800

    Time: 330 ms (65.56%), Space: 16.6 MB (67.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,29 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n=len(s)
-        dp=[[False]*n for _ in range(n)]
-        start,end=0,1 #나중에 결과 출력할 때, 어디서부터 어디까지 출력할지 표시할 index
         
-        #base cases
-        #문자열의 길이가 1인 경우 무조건 True
-        for i in range(n):
-            dp[i][i]=True
-            #문자열의 길이가 2인데, 두 글자 모두 같으면 True
-            #i<n-1가 있는 이유 : s[i+1] 의 범위도 생각해야하기 때문
-            if i<n-1 and s[i]==s[i+1]:
-                dp[i][i+1]=True
-                start=i
-                end=2
-
-        #reputation
-        #문자열의 길이가 3 이상인 경우
-        for length in range(3,n+1):
-            for i in range(n-length+1):
-                j=i+length-1
-                dp[i][j]=(s[i]==s[j]) and dp[i+1][j-1]
-                #가장 긴 문자열의 정보 갱신
-                if dp[i][j] and length>end:
-                    start=i
-                    end=length
-                    
-        return s[start:start+end]
+        def expand(stringm,left,right):
+            while left>0 and right<len(s)-1 and s[left-1]==s[right+1]:
+                left-=1
+                right+=1
+            return left, right, right-left+1
+        
+        max_len,left,right=0,0,0
+        for ind in range(len(s)-1):
+            
+            #length of palindrome is odd
+            l1, r1, max_len1 = expand(s,ind,ind)
+            
+            if max_len1>max_len:
+                max_len=max_len1
+                left,right=l1,r1
+            
+            #length of palindrome is even
+            if s[ind]==s[ind+1]:
+                l2,r2,max_len2=expand(s,ind,ind+1)
+                
+                if max_len2>max_len:
+                    max_len=max_len2
+                    left,right=l2,r2
+        
+        return s[left:right+1]
+        return answer
+        
