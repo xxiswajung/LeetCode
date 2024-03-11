6c7b9e345eef1ca3a9e5600a72e76d616c23b925
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-03-11 15:35:43 -0700

    Time: 42 ms (50.11%), Space: 17.2 MB (49.20%) - LeetHub

---
+++ 0900-rle-iterator/0900-rle-iterator.py
@@ -0,0 +1,27 @@
+class RLEIterator:
+
+    def __init__(self, encoding: List[int]):
+        self.arr=encoding
+        
+    def next(self, n: int) -> int:
+        num=-1
+        while self.arr and n>0:
+            if n<=self.arr[0]:
+                self.arr[0]-=n
+                n=0
+                num=self.arr[1]
+            else:
+                n-=self.arr[0]
+                self.arr[0]=0
+            if self.arr[0]==0:
+                self.arr.pop(0)
+                self.arr.pop(0)
+                
+        if n>0:
+            return -1
+        return num
+
+
+# Your RLEIterator object will be instantiated and called as such:
+# obj = RLEIterator(encoding)
+# param_1 = obj.next(n)
