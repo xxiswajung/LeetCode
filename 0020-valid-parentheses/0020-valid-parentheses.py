767ff9d02c8538242e3d5573a653c6b67bf18c34
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-03-13 23:09:11 -0700

    Time: 36 ms (51.92%), Space: 16.6 MB (78.17%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,36 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+#         stack=[]
+#         for i in range(len(s)):
+#             item=s[i]
+#             if item=='(' or item=='[' or item=='{':
+#                 stack.append(item)
+#             else:
+#                 if stack:
+#                     if (item==')' and stack[-1]=='(') or (item==']' and stack[-1]=='[') or (item=='}' and stack[-1]=='{'):
+#                         stack.pop()
+#                     else:
+#                         stack.append(item)
+#                 else :
+#                     stack.append(item)
+        
+#         if stack:
+#             return False
+#         else:
+#             return True
+        
+        
+        
         stack=[]
-        for i in range(len(s)):
-            item=s[i]
-            if item=='(' or item=='[' or item=='{':
-                stack.append(item)
+        mapping={')':'(','}':'{',']':'['}
+        
+        for char in s:
+            if stack and char in mapping:
+                item=stack.pop()
+                if mapping[char]!=item:
+                    return False
             else:
-                if stack:
-                    if (item==')' and stack[-1]=='(') or (item==']' and stack[-1]=='[') or (item=='}' and stack[-1]=='{'):
-                        stack.pop()
-                    else:
-                        stack.append(item)
-                else :
-                    stack.append(item)
+                stack.append(char)
         
         if stack:
             return False
