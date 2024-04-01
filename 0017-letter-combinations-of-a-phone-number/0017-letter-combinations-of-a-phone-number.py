308d9e60ea381105f5783cc7e87706de7bd5af08
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-04-01 11:56:06 -0700

    Time: 34 ms (67.40%), Space: 16.4 MB (89.29%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,32 +1,23 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        phone=[0]*10
-        phone[2]=['a','b','c']
-        phone[3]=['d','e','f']
-        phone[4]=['g','h','i']
-        phone[5]=['j','k','l']
-        phone[6]=['m','n','o']
-        phone[7]=['p','q','r','s']
-        phone[8]=['t','u','v']
-        phone[9]=['w','x','y','z']
+        phone={'2':['a','b','c'], '3':['d','e','f'], '4':['g','h','i'], '5':['j','k','l'], '6':['m','n','o'], '7':['p','q','r','s'], '8':['t','u','v'], '9':['w','x','y','z']}
+        tmp=''
+        answer=[]
         
-        global ans
-        ans=''
-        res=[]
+        if len(digits)==0:
+            return answer
         
         def DFS(v):
-            global ans
+            nonlocal tmp, answer
             if v==len(digits):
-                res.append(ans)
+                answer.append(tmp)
                 return
             else:
-                for x in phone[int(digits[v])]:
-                    ans+=x
+                item=digits[v]
+                for i in range(len(phone[item])):
+                    tmp+=phone[item][i]
                     DFS(v+1)
-                    ans=ans[:-1]
-        
-        if digits=="":
-            return []
-        else:
-            DFS(0)
-            return res
+                    tmp=tmp[:len(tmp)-1]
+        DFS(0)
+        return answer
+            
