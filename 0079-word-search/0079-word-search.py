8d2659b5bebf4cf9ca4d0a052e48dc040d126b3e
hwajung <52371573+xxiswajung@users.noreply.github.com>
2024-04-01 14:11:20 -0700

    Time: 3429 ms (78.84%), Space: 16.5 MB (71.69%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,35 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        m=len(board[0])
+        n=len(board)
         dx=[-1,0,1,0]
         dy=[0,1,0,-1]
-        m=len(board)
-        n=len(board[0])
         
-        def DFS(x,y,idx):
-            global cmp, flag
-            if idx==len(word):
+        def DFS(v,x,y):
+            if v==len(word):
                 return True
-            else:
-                for i in range(4):
-                    xx=x+dx[i]
-                    yy=y+dy[i]
-                    if idx<len(word) and 0<=xx<m and 0<=yy<n and visited[xx][yy]==0 and board[xx][yy]==word[idx]:
-                        visited[xx][yy]=1
-                        cmp+=board[xx][yy]
-                        if DFS(xx,yy,idx+1):
-                            return True
-                        visited[xx][yy]=0
-                        cmp=cmp[:-1]
-        global cmp
-        cmp=''
-        for i in range(m):
-            for j in range(n):
-                if board[i][j]==word[0]:
-                    visited=[[0]*n for _ in range(m)]
-                    visited[i][j]=1
-                    cmp+=board[i][j]
-                    if DFS(i,j,1):
+            for i in range(4):
+                xx=x+dx[i]
+                yy=y+dy[i]
+                if 0<=xx<n and 0<=yy<m and visited[xx][yy]==0 and board[xx][yy]==word[v]:
+                    visited[xx][yy]=1
+                    if DFS(v+1,xx,yy):
                         return True
-                    cmp=''
-        return False
+                    visited[xx][yy]=0
         
+        for x in range(n):
+            for y in range(m):
+                if board[x][y]==word[0]:
+                    visited = [[0]*m for _ in range(n)]
+                    visited[x][y]=1
+                    if DFS(1,x,y):
+                        return True
+        else:
+            return False
